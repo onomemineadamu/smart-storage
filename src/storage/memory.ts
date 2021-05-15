f7c96361e15fd11888da72c78921c898c235117a
Emmanuel Onah <emmajerry2016@gmail.com>
2021-05-15 20:13:47 +0300

    Update: initial idea for memory storage

--- src/storage/memory.ts
+++ src/storage/memory.ts
@@ -1,1 +1,26 @@
 export const checkMemory => () => {}
+
+
+function Memory(){
+    if(!new.target){
+        return arguments.callee()
+    }
+
+    setItem(key:string,value:any){
+        //storyewith memo consideration. Note every storage goes to smartStorageMemory ref key
+    }
+
+    getItem(key:string){
+        //get operation
+    }
+
+    removeItem(key:string){
+        //remove operation
+    }
+
+    clear(){
+        //delete smartStorageMemory that holds all other data as a key
+    }
+}
+
+export memory = Memory();
