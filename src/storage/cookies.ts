5c9c87d26416429a2596667cf1a699a14f5540a1
onome mine adamu <onomemineadamu@gmail.com>
2021-05-16 23:15:44 +0200

    Feature: Initial cookies storage implementation

--- src/storage/cookies.ts
+++ src/storage/cookies.ts
@@ -1,1 +1,19 @@
-export const checkCookies = () => {};
+export const checkCookies = () => {
+  try {
+    document.cookie = "SMART_STORAGE=TEST_COOKIE";
+  } catch (error) {}
+};
+
+class Cookies {
+  date = new Date();
+  setItem<T>(key: string, value: T, expiringDay: number = 1) {
+    this.date.setTime(this.date.getTime() + expiringDay * 24 * 60 * 60 * 1000);
+    const expires = `expires=${this.date.toUTCString()}`;
+    document.cookie = `${key}=${value};${expires};path=/`;
+  }
+
+  getItem(key: string) {
+    const name = `${key}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+  }
+}
