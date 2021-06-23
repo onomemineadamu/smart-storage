6c8aea9fd051229fc130a32f8c1490656202736d
Emmybest1 <emmajerry2016@gmail.com>
2021-06-24 02:25:45 +0300

    Feature: added SmartStorage default memory

--- src/storage/memory.ts
+++ src/storage/memory.ts
@@ -1,32 +1,74 @@
-export const checkMemory = () => { }
-
-function Memory(this: any) {
-    if (!new.target) {
-        return arguments.callee()
-    }
-
-    this.smartStorageMemory = {}
-
-
-    this.setItem = function (key: string, value: any) {
-        if (this.smartStorageMemory[key] !== value) {
-            this.smartStorageMemory[key] = value
-        }
-        return;
-    }
-
-    this.getItem = function (key: string) {
-        return this.smartStorageMemory[key]
-    }
-
-    this.removeItem = function (key: string) {
-        delete this.smartStorageMemory[key]
-    }
-
-    this.clear = function () {
-        delete this.smartStorageMemory;
-
-    }
+/* eslint-disable no-unused-vars */
+interface ISmartStorageMemory {
+  smartStorageMemory: {
+    [x: string]: any;
+  };
+  setItem: Function;
+  getItem: Function;
+  removeItem: Function;
+  clearItems: Function;
 }
 
-export const memory = Memory();
+const SmartStorageMemory: InstanceType<any> = function (this: ISmartStorageMemory) {
+  if (!new.target) {
+    return new SmartStorageMemory();
+  }
+
+  if (typeof SmartStorageMemory.instance === "object") {
+    return SmartStorageMemory.instance;
+  }
+
+  SmartStorageMemory.instance = this;
+  this.smartStorageMemory = {};
+
+  this.setItem = function (key: string, value: any) {
+    this.smartStorageMemory[key] = value;
+  };
+
+  this.getItem = function (key: string) {
+    if (!this.smartStorageMemory[key]) {
+      console.group("📦 SmartStorage🚨");
+      console.error(`Unrecognized data ${key} to get`);
+      console.groupEnd();
+      return;
+    }
+
+    return this.smartStorageMemory[key];
+  };
+
+  this.removeItem = function (key: string) {
+    if (!this.smartStorageMemory[key]) {
+      console.group("📦 SmartStorage🚨");
+      console.error(`Unrecognized data ${key} to remove`);
+      console.groupEnd();
+      return;
+    }
+
+    delete this.smartStorageMemory[key];
+  };
+
+  this.clearItems = function () {
+    this.smartStorageMemory = {};
+  };
+} as any as { new (): ISmartStorageMemory };
+
+const smartStorageMemory = new SmartStorageMemory();
+
+// positive check
+smartStorageMemory.setItem("name", "Jeremiah Joseph");
+console.log("smartStorageMemory", smartStorageMemory);
+
+// positive check
+console.log("smartStorageMemory", smartStorageMemory.getItem("name"));
+
+// negative check
+console.log("smartStorageMemory", smartStorageMemory.getItem("n"));
+
+// negative check
+console.log("smartStorageMemory", smartStorageMemory.removeItem("n"));
+
+const sigletonTest = new SmartStorageMemory();
+console.log("Sigleton Test", sigletonTest);
+
+// psitive check
+smartStorageMemory.clearItems();
